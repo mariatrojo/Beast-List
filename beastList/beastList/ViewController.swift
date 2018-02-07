2d52c699e6ba58a9b90266d6e746dc359ab81d6e
mariarojo <mariatrojo@gmail.com>
2018-02-06 18:22:41 -0800

    adding beast list project

---
+++ beastList/beastList/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  beastList
+//
+//  Created by Maria Teresa Rojo on 1/12/18.
+//  Copyright © 2018 Maria Rojo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var tasks = ["Something cool", "Something VERY cool", "Something EXTREMELY cool"]
+    
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        if taskTextField.text != "" {
+            tasks.append(taskTextField.text!)
+            tableView.reloadData()
+            print(taskTextField.text!)
+            taskTextField.text = ""
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Section: \(indexPath.section) and Row: \(indexPath.row)")
+        tasks.remove(at: indexPath.row)
+        tableView.reloadData()
+    }
+}
+
